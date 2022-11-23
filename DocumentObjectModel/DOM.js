48da13acfac055755754290297a741370ba07552
Álife Silva De Moraes <alifesilva90@gmail.com>
2022-11-23 17:53:00 -0300

    Documento Object Model Resumo

---
+++ DocumentObjectModel/DOM.js
@@ -0,0 +1,12 @@
+//DOCUMENT OBJECT MODEL PROPRIEDADES
+console.log(document.title + 'Título do documento atual')
+console.log(document.lastModified + 'Data em que o documento foi modificado pela ultima vez')
+console.log(document.URL + 'Retorna uma string contendo a URL do documento atual')
+console.log(document.domain + 'Retorna o dominio do documento atul')
+//DOCUMENT OBJECT MODEL METODOS
+document.write() // Grava um texto no documento
+document.getElementById() // Retorna o elemento pelo seu id, especificado no parametro
+document.querySelectorAll()//Retorna uma lista de elementos que correspodem a um seletor de CSS
+document.createElement()// Cria um novo elemento
+document.createTextNode() // Cria um novo nó de texto
+document.appendChild() // adiciona uma elemento filho ao pai
